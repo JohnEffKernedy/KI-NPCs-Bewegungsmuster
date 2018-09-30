cbf93d830d6b0057618f6fcb2cb3ff2aa80b5ea4
JohnEffKernedy <florian.kaindl@stud.uni-regensburg.de>
2018-09-30 15:16:39 +0200

    Spawnrate public

--- Assets/_Own/Flock/Scripts/SpawnManager.cs
+++ Assets/_Own/Flock/Scripts/SpawnManager.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour {
 
     public List<WaypointCluster> spawner;
+    public float spawnRate = 20f;
 
 	// Use this for initialization
 	void Start () {
         // for demo purposes, spawn rates should be set from AI Manager
-        InvokeRepeating("ActivateRandomSpawner", 2, 30);
+        InvokeRepeating("ActivateRandomSpawner", 2, spawnRate);
     }
 	
 	// Update is called once per frame
